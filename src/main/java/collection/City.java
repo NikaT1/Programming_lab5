18a0658106788e218c67d01564bc24d7b6b19e5f
Troynikova Veronika <troynikv@gmail.ru>
2021-03-03 21:26:03 +0300

    Fix CommandSave

--- src/main/java/collection/City.java
+++ src/main/java/collection/City.java
@@ -107,23 +107,6 @@ public class City {
     public Human getGovernor() {
         return governor;
     }
-
-    /**
-     * public int hashCode() {
-     * int r = Objects.hashCode(getName());
-     * return r;
-     * }
-     * public boolean equals(Object entity1) {
-     * if (entity1 == this) {
-     * return true;
-     * }
-     * if (entity1 == null || getClass() != entity1.getClass()) {
-     * return false;
-     * }
-     * Entity entity2 = (Entity) entity1;
-     * return getName() == entity2.getName();
-     * }
-     */
     public String toString() {
         return getId() + ", название: " + getName() + ", " +
                 coordinates.toString() + ", дата создания: " +
--- src/main/java/commands/CommandSave.java
+++ src/main/java/commands/CommandSave.java
@@ -5,20 +5,29 @@ import collection.CreationPriorityQueue;
 import IOutils.InputAndOutput;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 public class CommandSave extends Commands {
+    private PriorityQueue<City> dop = new PriorityQueue<City>(10, new Comparator<City>(){
+        public int compare(City c1, City c2) {
+            return (c2.getArea() - c1.getArea());
+        }
+    });
     public CommandSave() {
         super("save", "сохранить коллекцию в файл");
     }
     public void doCommand(InputAndOutput inputAndOutput, CommandsControl commandsControl, CreationPriorityQueue priorityQueue) {
         try {
             PrintWriter printWriter = new PrintWriter(priorityQueue.getFilePath());
-            printWriter.write("name,x,y,area,population,metersAboveSeaLevel,establishmentDate,agglomeration,climate,age" + "\n");
+            printWriter.write("id,name,x,y,creationDate,area,population,metersAboveSeaLevel,establishmentDate,agglomeration,climate,age" + "\n");
             while (!priorityQueue.getPriorityQueue().isEmpty()){
                 City city = priorityQueue.pollFromQueue();
+                printWriter.write(city.getId()+",");
                 printWriter.write(city.getName()+",");
                 printWriter.write(city.getCoordinates().getX()+",");
                 printWriter.write(city.getCoordinates().getY()+",");
+                printWriter.write(city.getCreationDate()+",");
                 printWriter.write(city.getArea()+",");
                 printWriter.write(city.getPopulation()+",");
                 printWriter.write(city.getMetersAboveSeaLevel()+",");
@@ -27,10 +36,14 @@ public class CommandSave extends Commands {
                 printWriter.write(city.getClimate()+",");
                 printWriter.write(city.getGovernor().getAge() + "");
                 printWriter.write("\n");
+                dop.add(city);
             }
             printWriter.flush();
+            while (!dop.isEmpty()) {
+                priorityQueue.addToQueue(dop.poll());
+            }
         } catch (FileNotFoundException e) {
-            inputAndOutput.output("Файл не существует или не хватает прав на чтение");
+            inputAndOutput.output("Не хватает прав на запись в файл");
         }
     }
 }
